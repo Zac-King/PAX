192148e4131768e60e31091bb699b5f7464c3713
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-11-11 16:43:31 -0600

    Made the script  a base class

--- Assets/Scripts/BroadcastOncollisionEnter.cs
+++ Assets/Scripts/BroadcastOncollisionEnter.cs
@@ -5,7 +5,7 @@ public class BroadcastOncollisionEnter : MonoBehaviour
 {
 	[SerializeField] string message;
 
-	void OnCollisionEnter(Collision c)
+	protected virtual void OnCollisionEnter(Collision c)
 	{
 		Messenger.Broadcast<string>(message,c.gameObject.GetInstanceID().ToString());
 
