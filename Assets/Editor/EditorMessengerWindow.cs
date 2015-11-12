30d6167f7e403f8875766d2be6d059377ae237b3
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-11-12 11:31:22 -0600

    Finished the Editor Window for Messenger.

---
+++ Assets/Editor/EditorMessengerWindow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Developed by: Quinton "Kiro" Baudoin
+/// 
+/// Purpose:
+/// 	Creates an editor window to show the messages that have been listened for.
+/// 
+/// 
+/// 
+/// 
+/// </summary>
+
+public class MessengerWindow : EditorWindow 
+{
+	[MenuItem("Messenger/Messages")]
+	static public void MakeWindow()
+	{
+		EditorWindow.GetWindow(typeof(MessengerWindow));
+	}
+	public void OnGUI()
+	{
+		string messages = null;
+
+		//EditorWindow.GetWindow(typeof(MessengerWindow));
+
+		if(StaticMessengerGetter.MessengerListenerList().Count > 0)
+		{
+		foreach(string s in StaticMessengerGetter.MessengerListenerList())
+		{
+			if(messages == null)
+					messages = s;
+
+			else 
+					messages += Environment.NewLine + s;
+		}
+		}
+		else
+			messages = Environment.NewLine + "<<Empty>>";
+
+		EditorGUILayout.LabelField("Listening For: ",EditorStyles.boldLabel);
+		EditorGUILayout.LabelField(messages);
+
+		if(GUILayout.Button("Scan"))
+			OnGUI();
+
+	
+	
+	}
+
+	void Rescan()
+	{
+		OnGUI();
+	}
+}
