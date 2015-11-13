344d78482202496af16ae971e2ae82ec09bf163c
slanden <slanden523@gmail.com>
2015-11-12 18:01:36 -0600

    Shelby
    -CameraListener adjusted
    -SetPlayerPosition
    -SlidingDoor

--- Assets/Scripts/SlidingDoor.cs
+++ Assets/Scripts/SlidingDoor.cs
@@ -4,16 +4,30 @@ using System.Collections;
 public class SlidingDoor : MonoBehaviour
 {
     public string listeningFor;
+    string activator;
+    bool locked = false;
+
     void Awake()
     {
         Messenger.AddListener<GameObject, string>(listeningFor, Activate);
+
+        BoxCollider[] children = GetComponentsInParent<BoxCollider>();
+        foreach (Collider c in children)
+        {
+            if (c.isTrigger == true)
+            {
+                activator = c.name;
+                return;
+            }
+        }
     }
 
-    void Activate(GameObject o,string s)
+    void Activate(GameObject o,string broadcaster)
     {
-        if(o.tag == "Player")
+        if(o.tag == "Player" && locked == false && broadcaster == activator)
         {
             transform.position += new Vector3(0, 5, 0);
+            locked = true;
         }
     }
 
