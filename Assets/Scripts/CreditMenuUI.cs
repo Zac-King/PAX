eaa5a7504e924b11559d7c67c41e1f6045afd4dd
coringuyen <coringuyen96@gmail.com>
2015-11-13 17:50:48 -0600

    gui changes

--- Assets/Scripts/CreditMenuUI.cs
+++ Assets/Scripts/CreditMenuUI.cs
@@ -16,10 +16,13 @@ public class CreditMenuUI : MonoBehaviour {
 		textPos = Programmers.transform.position;
 		StartCoroutine ("credits");
 
+	}
+
+	void Awake()
+	{
 		MenuSystem.AddPrefab ("start->end", gameObject);	
 		MenuSystem.ListenerToTransition ("gamestatechanged");
 	}
-	
 
 	IEnumerator credits() 
 	{
