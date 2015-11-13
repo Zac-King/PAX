6720f647fc70a9ad5780fe839773542663f81006
Chui <lordbabypanda@gmail.com>
2015-11-13 10:09:05 -0600

    Menu
    Added GameOver and changed StartMenu

--- Assets/Scripts/MenuSystem.cs
+++ Assets/Scripts/MenuSystem.cs
@@ -5,20 +5,20 @@ using System.Collections.Generic;
 
 static public class MenuSystem
 {
-	static Dictionary<string, GameObject> menus = new Dictionary<string, GameObject> ();
+	static Dictionary<string, GameObject> menus = new Dictionary<string, GameObject>();
 	
 	static public void AddPrefab(string argument, GameObject prefab)
 	{
-		menus.Add (argument, prefab);
+		menus.Add(argument, prefab);
 	}
 	
 	static public void ListenerToTransition(string message)
 	{
-		Messenger.AddListener<string> (message, selfActivate);
+		Messenger.AddListener<string>(message, selfActivate);
 	}
 	
 	static private void selfActivate(string argument)
 	{
-		menus [argument].SetActive (true);
+		menus [argument].SetActive(true);
 	}
 }
